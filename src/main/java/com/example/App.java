b2864b37fa4388143e426a8127513130e47687fe
rumburakgit <rumburak.eclipse@gmail.com>
2023-03-10 10:39:19 +0100

    Update App.java
    add

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -18,7 +18,7 @@ public class App {
     public void init()
     {
         Logger log = LoggerFactory.getLogger(App.class);
-        log.info("Java app started");
+        log.info("Java app started 3");
     }
 
     public String getStatus() {
